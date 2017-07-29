f7ec5da07d292a5e71d512759089b0516b7e888e
yolorusher <yolorusher@gmail.com>
2017-07-30 01:05:18 +0300

    Code style changes

--- Program.cs
+++ Program.cs
@@ -2,39 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace LinqLinqLinq {
-    class Program {
-        static void Main(string[] args) {
-            IEnumerable<Table> table = new List<Table> {
-                new Table{Id = 123, Category = "Игры", LastName = "Иванов", Address = "Калининград"},
-                new Table{Id = 128, Category = "Спорт", LastName = "Иванов", Address = "Калининград"},
-                new Table{Id = 124, Category = "Игры", LastName = "Иванов", Address = "Москва"},
-                new Table{Id = 125, Category = "Игры", LastName = "Сидоров", Address = "Екатеринбург"},
-                new Table{Id = 126, Category = "Спорт", LastName = "Петров", Address = "Уфа"},
-                new Table{Id = 127, Category = "Спорт", LastName = "Сидоров", Address = "Владивосток"},
-            };
+namespace LinqLinqLinq
+{
+  class Table
+  {
+    public int Id { get; set; }
+    public string Category { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+  }
+  class Program
+  {
+    IEnumerable<Table> table = new List<Table>
+    {
+      new Table{Id = 123, Category = "Игры", LastName = "Иванов", Address = "Калининград"},
+      new Table{Id = 128, Category = "Спорт", LastName = "Иванов", Address = "Калининград"},
+      new Table{Id = 124, Category = "Игры", LastName = "Иванов", Address = "Москва"},
+      new Table{Id = 125, Category = "Игры", LastName = "Сидоров", Address = "Екатеринбург"},
+      new Table{Id = 126, Category = "Спорт", LastName = "Петров", Address = "Уфа"},
+      new Table{Id = 127, Category = "Спорт", LastName = "Сидоров", Address = "Владивосток"},
+    };
 
-            var result = table.GroupBy(x => x.Category).Select(x => x.Distinct(y => y.LastName).Count());
+    static void Main(string[] args)
+    {
+      var result = table.GroupBy(x => x.Category)
+        .Select(x => x.Distinct(y => y.LastName)
+        .Count());
 
-            foreach (var item in result) 
-                Console.WriteLine(item);
+      foreach (var item in result)
+      {
+        Console.WriteLine(item);
+      }
+    }
+  }
+
+  static class Extensions
+  {
+    public static IEnumerable<T> Distinct<T, U>(this IEnumerable<T> source, Func<T, U> selector)
+    {
+      var set = new HashSet<U>();
+
+      foreach (var item in source)
+      {
+        if (set.Add(selector(item)))
+        {
+          yield return item;
         }
+      }
     }
-
-    class Table {
-        public int Id { get; set; }
-        public string Category { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-    }
-
-    static class Extensions {
-        public static IEnumerable<T> Distinct<T, U>(this IEnumerable<T> source, Func<T, U> selector) {
-            var set = new HashSet<U>();
-
-            foreach (var item in source) 
-                if (set.Add(selector(item))) 
-                    yield return item;
-        }
-    }
+  }
 }
